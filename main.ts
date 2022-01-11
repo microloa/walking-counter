f1cedea02d1414ef97bd4b3224574c10ac2de75c
microloa <97388308+microloa@users.noreply.github.com>
2022-01-11 21:34:43 +0900

    press B to mute

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 let count = 0
+let mute = 0
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(count)
 })
@@ -6,7 +7,17 @@ input.onButtonPressed(Button.AB, function () {
     count = 0
     basic.showNumber(count)
 })
+input.onButtonPressed(Button.B, function () {
+    if (mute == 0) {
+        mute = 1
+    } else {
+        mute = 0
+    }
+})
 input.onGesture(Gesture.Shake, function () {
     count += 1
-    music.playTone(Note.C, music.beat())
+    if (!(mute == 1)) {
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+    }
+    basic.showNumber(count)
 })
